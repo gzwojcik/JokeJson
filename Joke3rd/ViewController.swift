cf6f3507db4d3f462b1c448dfc7912e918c81d7e
Grzegorz Wojcik <Gzwojcik@icloud.com>
2020-05-02 19:23:40 +0200

    Initial Commit

---
+++ Joke3rd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Joke3rd
+//
+//  Created by zgaga on 02/05/2020.
+//  Copyright © 2020 Hime Electronics Int. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
